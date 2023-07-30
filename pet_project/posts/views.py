ebd999b7dfe1f94f5e12a902ea2b4db55b33546b
vasilevegor <vasilev.egor163@gmail.com>
2023-07-30 20:20:17 +0200

    add 2 pages and css

--- pet_project/posts/views.py
+++ pet_project/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'This is main page of Pet project'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = f'Here you will see info about groups + {slug}'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
+
